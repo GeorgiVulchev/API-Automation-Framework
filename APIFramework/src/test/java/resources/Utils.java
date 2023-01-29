24b7c035aff35a03d69d103d87fc24b57d7e8bf9
Georgi Valchev <georgivvulchev@gmail.com>
2023-01-29 12:28:11 +0200

    Created getJsonPath method

--- APIFramework/src/test/java/resources/Utils.java
+++ APIFramework/src/test/java/resources/Utils.java
@@ -10,6 +10,8 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Utils {
@@ -34,5 +36,13 @@ public class Utils {
 				"C:\\Users\\GValchev\\git\\API-Automation-Framework\\APIFramework\\src\\test\\java\\resources\\global.properties");
 		prop.load(fis);
 		return prop.getProperty(key);
+	}
+	
+	public String getJsonPath(Response response, String key) {
+		String res = response.asString();
+		JsonPath js = new JsonPath(res);
+		
+		return js.get(key);
+
 	}
 }
