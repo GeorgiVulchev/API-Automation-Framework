ef3aad35a08ed9b221f9cf83a0e3d6a2866c0177
Georgi Valchev <georgivvulchev@gmail.com>
2023-01-29 15:22:32 +0200

    Created Delete place payload method.

--- APIFramework/src/test/java/stepDefinitions/stepDefinition.java
+++ APIFramework/src/test/java/stepDefinitions/stepDefinition.java
@@ -24,6 +24,7 @@ public class stepDefinition extends Utils {
 	Response response;
 	TestDataBuild data = new TestDataBuild();
 	JsonPath js;
+	static String place_id;
 
 	@Given("Add Place Payload with {string} {string} {string}")
 	public void add_place_payload_with(String name, String language, String address) throws IOException {
@@ -35,8 +36,9 @@ public class stepDefinition extends Utils {
 		APIResources resourceAPI = APIResources.valueOf(resource);
 		resourceAPI.getResource();
 
-		//responseSpecification = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON)
-		//		.build();
+		// responseSpecification = new
+		// ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON)
+		// .build();
 
 		if (httpMethod.equalsIgnoreCase("POST"))
 			response = request.when().post(resourceAPI.getResource());
@@ -53,14 +55,20 @@ public class stepDefinition extends Utils {
 	public void the_in_the_response_body_is(String keyValue, String expectedValue) {
 		assertEquals(getJsonPath(response, keyValue), expectedValue);
 	}
-	
+
 	@Then("verify place_id created maps to {string} using {string}")
 	public void verify_place_id_created_maps_to_using(String expectedName, String resource) throws IOException {
-		String place_id = getJsonPath(response, "place_id");
+		place_id = getJsonPath(response, "place_id");
 		request = given().spec(requestSpecification()).queryParam("place_id", place_id);
-		
+
 		user_calls_with_http_request(resource, "GET");
 		String actualName = getJsonPath(response, "name");
 		assertEquals(actualName, expectedName);
 	}
+
+	@Given("DeletePlace payload")
+	public void delete_place_payload() throws IOException {
+		request = given().spec(requestSpecification()).body(data.deletePlacePayload(place_id));
+	}
+
 }
