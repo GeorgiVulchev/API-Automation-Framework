be68c7e3041b435e23eb19c655873f495314d08e
Georgi Valchev <georgivvulchev@gmail.com>
2023-01-29 12:33:37 +0200

    Implemented the verification step and optimized the the_in_the_response_body_is method

--- APIFramework/src/test/java/stepDefinitions/stepDefinition.java
+++ APIFramework/src/test/java/stepDefinitions/stepDefinition.java
@@ -23,6 +23,7 @@ public class stepDefinition extends Utils {
 	ResponseSpecification responseSpecification;
 	Response response;
 	TestDataBuild data = new TestDataBuild();
+	JsonPath js;
 
 	@Given("Add Place Payload with {string} {string} {string}")
 	public void add_place_payload_with(String name, String language, String address) throws IOException {
@@ -34,8 +35,8 @@ public class stepDefinition extends Utils {
 		APIResources resourceAPI = APIResources.valueOf(resource);
 		resourceAPI.getResource();
 
-		responseSpecification = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON)
-				.build();
+		//responseSpecification = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON)
+		//		.build();
 
 		if (httpMethod.equalsIgnoreCase("POST"))
 			response = request.when().post(resourceAPI.getResource());
@@ -50,8 +51,16 @@ public class stepDefinition extends Utils {
 
 	@Then("the {string} in the response body is {string}")
 	public void the_in_the_response_body_is(String keyValue, String expectedValue) {
-		String res = response.asString();
-		JsonPath js = new JsonPath(res);
-		assertEquals(js.get(keyValue).toString(), expectedValue);
+		assertEquals(getJsonPath(response, keyValue), expectedValue);
+	}
+	
+	@Then("verify place_id created maps to {string} using {string}")
+	public void verify_place_id_created_maps_to_using(String expectedName, String resource) throws IOException {
+		String place_id = getJsonPath(response, "place_id");
+		request = given().spec(requestSpecification()).queryParam("place_id", place_id);
+		
+		user_calls_with_http_request(resource, "GET");
+		String actualName = getJsonPath(response, "name");
+		assertEquals(actualName, expectedName);
 	}
 }
